a1e7cde86776d53ebee5c0091185bd77fabd71ef
nandhini-stars <240196.it@rmkec.ac.in>
2026-02-24 17:21:33 +0530

    Update with Signup Login.js
    Added Sigup and updated login.js

--- frontend/RailX/src/Login.js
+++ frontend/RailX/src/Login.js
@@ -1,40 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
-import logo from "./railx.png"; // Put your image inside src folder
+import railImage from "./railx.png";
 
 function Login() {
   return (
-    <div className="main-container">
-      <div className="card">
-
-        {/* Left Side */}
-        <div className="left-section">
-          <img src={logo} alt="RailX Logo" className="logo" />
-        </div>
-
-        {/* Right Side */}
-        <div className="right-section">
-          <h1>LOGIN</h1>
-
-          <label>Email:</label>
-          <input type="email" placeholder="Enter your email" />
-
-          <label>Password:</label>
-          <input type="password" placeholder="Enter your password" />
-
-          <button className="login-btn">Login</button>
-
-          <div className="extra-links">
-            <span>Remember me</span>
-            <span>Forgot Password?</span>
-          </div>
-
-          <p className="signup-text">
-            Don’t have an account? <span className="signup-link">Sign Up</span>
-          </p>
-        </div>
-
+    <div className="login-container">
+      
+      <div className="login-left">
+        <img src={railImage} alt="RailX" />
       </div>
+
+      <div className="login-right">
+        <h1>LOGIN</h1>
+
+        <label>Email:</label>
+        <input type="email" placeholder="Enter your email" />
+
+        <label>Password:</label>
+        <input type="password" placeholder="Enter your password" />
+
+        <button className="login-btn">Login</button>
+
+        <div className="login-links">
+          <span>Remember me</span>
+          <span>Forgot Password?</span>
+        </div>
+
+        <p className="signup-text">
+          Don’t have an account?{" "}
+          <Link to="/signup" className="signup-link">
+            Sign Up
+          </Link>
+        </p>
+      </div>
+
     </div>
   );
 }
