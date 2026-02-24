b9e18b6b6fb0fd2709ef9a3c233f907cff5c401c
nandhini-stars <240196.it@rmkec.ac.in>
2026-02-24 17:20:07 +0530

    Update with Sigup App.js
    Added SignUp Page

--- frontend/RailX/src/App.js
+++ frontend/RailX/src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import Signup from "./Signup";
 
 function App() {
-  return <Login />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
